c9db33cbfc72a91e9b388e002092c5de4a91ca53
Austin Cohrs <acohrsdev@outlook.com>
2017-10-24 10:43:58 -0700

    Added ssl

--- IdentityDay2/Startup.cs
+++ IdentityDay2/Startup.cs
@@ -13,23 +13,18 @@ using IdentityDay2.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace IdentityDay2
 {
     public class Startup
     {
-        //Configuration for User Secrets
         string _testSecret = null;
-        public Startup(IHostingEnvironment env)
+
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder();
-
-            if (env.IsDevelopment())
-            {
-                builder.AddUserSecrets<Startup>();
-            }
-
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
         //Configuration setup for dependancy injection
@@ -40,10 +35,16 @@ namespace IdentityDay2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
             _testSecret = Configuration["MySecret"];
 
             //Enable Admin-Only policy
-            services.AddAuthorization(options => {
+            services.AddAuthorization(options =>
+            {
                 options.AddPolicy("Admin Only", policy => policy.RequireRole("Admin"));
                 options.AddPolicy("Medical", policy => policy.Requirements.Add(new MedicalOfficerRequirement()));
             });
@@ -69,6 +70,10 @@ namespace IdentityDay2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var options = new RewriteOptions()
+                          .AddRedirectToHttps();
+
+            app.UseRewriter(options);
             //Enable user profiles & authorization via the Identity API
             app.UseAuthentication();
             if (env.IsDevelopment())
@@ -78,7 +83,6 @@ namespace IdentityDay2
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
 
-            //verify successfull user secret
             var result = string.IsNullOrEmpty(_testSecret) ? "Null" : "Not Null";
             app.Run(async (context) =>
             {
