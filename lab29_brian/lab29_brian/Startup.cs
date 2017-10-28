bb24d3ec63b078723f459edc5a353ea2bff1fb19
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-28 10:18:19 -0700

    forgot to rename my project so I recreated it

--- lab29_brian/lab29_brian/Startup.cs
+++ lab29_brian/lab29_brian/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WebApplication1
+namespace lab29_brian
 {
     public class Startup
     {
